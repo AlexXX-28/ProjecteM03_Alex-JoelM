c2bcaebcc1d80f58de346ccef41c471258738b46
DESKTOP-0IBBT46\Insti <j.maria@sapalomera.cat>
2020-12-28 17:36:32 +0100

    first prova joel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("La vi e vel for me");
-	// write your code here
+	//MENU
+        Scanner s = new Scanner(System.in);
+        System.out.println("1) Carrega taulell\n2) Introduir malalts\n3) Transmitir Virus\n4) Curar malats\n5) Desplaçar malalts\n6) Mostrar informació\n7) Sortir\nIntroduiex un numero del 1 al 7 per seleccionar l'opcio: ");
+        int numOpcio = s.nextInt();
+
     }
 }
