08398ea144a930e6382c5590bb098558b3bc5dcf
DESKTOP-0IBBT46\Insti <j.maria@sapalomera.cat>
2020-12-28 17:52:02 +0100

    primer versio del menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,15 @@ public class Main {
     public static void main(String[] args) {
 	//MENU
         Scanner s = new Scanner(System.in);
-        System.out.println("1) Carrega taulell\n2) Introduir malalts\n3) Transmitir Virus\n4) Curar malats\n5) Desplaçar malalts\n6) Mostrar informació\n7) Sortir\nIntroduiex un numero del 1 al 7 per seleccionar l'opcio: ");
+        System.out.printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s",
+                            "1) Carrega taulell",
+                            "2) Introduir malalts",
+                            "3) Transmitir Virus",
+                            "4) Curar malats",
+                            "5) Desplaçar malalts",
+                            "6) Mostrar informació",
+                            "7) Sortir",
+                            "Introduiex un numero del 1 al 7 per seleccionar l'opcio:");
         int numOpcio = s.nextInt();
 
     }
